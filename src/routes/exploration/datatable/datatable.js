675bb3374a203c8162eecc65768f9c0065259fc0
Julie Chen <juliechenxoxo@gmail.com>
2018-04-19 21:09:57 -0400

    datatable and scatter

--- src/routes/exploration/datatable/datatable.js
+++ src/routes/exploration/datatable/datatable.js
@@ -3,174 +3,48 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 const title = 'Data Table View';
 
+const moodType = {
+  0: 'good',
+  1: 'bad',
+  2: 'unknown',
+};
+
+function enumFormatter(cell, row, enumObject) {
+  return enumObject[cell];
+}
+const products = [{
+  id: 1,
+  date: '2018/04/05',
+  time: '08:15',
+  mood: 0,
+  score: 10,
+  text: '....',
+  audio: 'audio',
+}, {
+  id: 2,
+  date: '2018/05/06',
+  time: '09:15',
+  mood: 0,
+  score: 8,
+  text: '....',
+  audio: 'audio',
+}];
 class DataTablePage extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      goodMoodData: [
-        [
-          '2018/04/05',
-          '08:15',
-          10.3,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/05',
-          '09:15',
-          6.3,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/07',
-          '12:16',
-          6.9,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/09',
-          '19:30',
-          3.3,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/09',
-          '19:50',
-          4.3,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/10',
-          '10:20',
-          7.3,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/11',
-          '09:20',
-          6.1,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/13',
-          '09:56',
-          7.7,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/13',
-          '15:20',
-          2.3,
-          'GOOD',
-          'audio',
-        ],
-        [
-          '2018/04/13',
-          '17:15',
-          8.8,
-          'GOOD',
-          'audio',
-        ],
-      ],
-      badMoodData: [
-        [
-          '2018/04/03',
-          '08:15',
-          -3.3,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/03',
-          '19:15',
-          -5.3,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/04',
-          '12:16',
-          -6.5,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/08',
-          '13:30',
-          -3.4,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/08',
-          '20:50',
-          -4.3,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/09',
-          '10:21',
-          -7.0,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/10',
-          '01:20',
-          -5.1,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/12',
-          '21:56',
-          -3.7,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/15',
-          '15:23',
-          -2.9,
-          'BAD',
-          'audio',
-        ],
-        [
-          '2018/04/15',
-          '17:40',
-          -8.3,
-          'BAD',
-          'audio',
-        ],
-      ],
-    };
   }
 
   render() {
-    const products = [{
-      id: 1,
-      name: 'Item name 1',
-      price: 100,
-    }, {
-      id: 2,
-      name: 'Item name 2',
-      price: 100,
-    }];
-
     return (
       <div>
         <BootstrapTable data={products} striped hover>
-          <TableHeaderColumn dataField="id" isKey dataAlign="center" dataSort>Product  ID</TableHeaderColumn>
-          <TableHeaderColumn dataField="name" dataSort>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField="id" isKey dataSort>Data ID</TableHeaderColumn>
+          <TableHeaderColumn dataField="date" dataSort>Date</TableHeaderColumn>
+          <TableHeaderColumn dataField="time" dataSort>Time</TableHeaderColumn>
+          <TableHeaderColumn dataField="mood" filterFormatted dataFormat={enumFormatter} formatExtraData={moodType} filter={{ type: 'SelectFilter', options: moodType, defaultValue: 1 }}>Mood</TableHeaderColumn>
+          <TableHeaderColumn dataField="score" dataSort>Score</TableHeaderColumn>
+          <TableHeaderColumn dataField="text">Text</TableHeaderColumn>
+          <TableHeaderColumn dataField="audio">Audio</TableHeaderColumn>
         </BootstrapTable>
       </div>
     );
--- src/routes/exploration/scatter/scatter.js
+++ src/routes/exploration/scatter/scatter.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import { ScatterChart, Scatter, Cell, XAxis, YAxis, CartesianGrid, LabelList, Tooltip, ReferenceLine, Label } from 'recharts';
 
 const title = 'Scatter Data';
+
+// const { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, LabelList, Tooltip} = Recharts;
+const data = [{ x: -100, y: 200, z: 200}, { x: 120, y: 100, z: 260 },
+  { x: 170, y: -300, z: 400}, {x: 140, y: 250, z: 280 },
+  { x: 150, y: 400, z: 500}, {x: 110, y: 280, z: 200 }]
+
+const colors = ['red', 'green', 'pink', 'yellow'];
 
 class ScatterPage extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  render() {``
+  displayName: 'ScatterExample';
+  render() {
     return (
-      <div className="col-lg-12">
-        <h1>Test</h1>
-      </div>
+      <ScatterChart width={600} height={600} margin={{ top: 50, right: 100, bottom: 50, left: 50 }}>
+        <XAxis height={90} type="number" dataKey={'x'} name="stature" unit="cm" >
+          <Label value="Pages of my website" offset={0} position="insideBottom" />
+        </XAxis>
+        <YAxis label={{ value: 'pv of page', angle: -90, position: 'insideLeft' }} type="number" dataKey={'y'} name="weight" unit="kg" />
+        <CartesianGrid />
+        <ReferenceLine x={0} stroke="red" label="Activity" />
+        <ReferenceLine y={0} label="Pleasant" stroke="red" />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter name="A school" data={data} fill="#8884d8">
+          {
+            data.map((entry, index) => {
+              return <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />;
+            })
+          }
+        </Scatter>
+      </ScatterChart>
     );
   }
 }
